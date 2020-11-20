a8d5240051fd232fd23bfd362e731ec1b6fc2201
unknown <burkekbur>
2020-11-20 19:12:29 +0100

    Oprava js na přepis primární barvy

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ let html = document.documentElement;
 document.querySelector('img').addEventListener('click', function ()
 {
   if (i == 0) {
-    html.style.setProperty('--secondary', '#7d0000');
+    html.style.setProperty('--primary', '#7d0000');
     html.style.setProperty('--secondary', 'gold');
     ++i;
   } else if (i == 1) {
-    html.style.setProperty('--secondary', 'black');
+    html.style.setProperty('--primary', 'black');
     html.style.setProperty('--secondary', 'lime');
     ++i;
   } else {
-    html.style.setProperty('--secondary', '#123');
+    html.style.setProperty('--primary', '#123');
     html.style.setProperty('--secondary', '#bada55');
     i = 0;
   }
