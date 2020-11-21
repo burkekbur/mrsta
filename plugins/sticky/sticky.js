e73b92db429a4af565c65aa1bec19da4ea1cd0a3
unknown <burkekbur>
2020-11-21 19:27:00 +0100

    Změna struktury

---
+++ plugins/sticky/sticky.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function (event) {
+
+  if (window.innerWidth > 750) {
+    let vyjizdec = document.createElement('vyjizdec');
+    vyjizdec.classList.add('vyjizdec')
+    vyjizdec.innerHTML = `
+          <div class="vyjizdec-header">
+          <span>Změnit barvy</span>
+          </div>
+          <div class="vyjizdec-content">
+            <h2>Změnit barevné téma</h2>
+            <div class="barvy">
+              <span onclick="html.style.setProperty('--primary', '#123');
+              html.style.setProperty('--secondary', '#bada55')" class="barva item-1"></span>
+              <span onclick="html.style.setProperty('--primary', '#7d0000');
+              html.style.setProperty('--secondary', 'gold');" class="barva item-2"></span>
+              <span onclick="html.style.setProperty('--primary', 'black');
+              html.style.setProperty('--secondary', 'lime');" class="barva item-3"></span>
+              <span title="Připravuje se" class="barva item-4 disable"></span>
+            </div>
+          </div>
+    `
+    document.body.appendChild(vyjizdec)
+  }
+
+  if ( /*@cc_on!@*/ true) {
+    let ieclass = 'ie' + document.documentMode;
+    document.querySelector('vyjizdec').classList.add('ie', ieclass);
+  }
+  let contWidth = document.querySelector('.vyjizdec-content').offsetWidth + 2;
+  document.querySelector('.vyjizdec').style.setProperty('left', '-' + contWidth + 'px');
+  document.querySelector('.vyjizdec').style.setProperty('visibility', 'visible');
+  let vyjizdecHeader = document.querySelectorAll(".vyjizdec-header");
+  for (let i = 0; i < vyjizdecHeader.length; i++) {
+    vyjizdecHeader[i].addEventListener('click', function () {
+      if (vyjizdecHeader[i].parentElement.classList.contains('open')) {
+        vyjizdecHeader[i].parentElement.style.setProperty('left', '-' + contWidth + 'px');
+        vyjizdecHeader[i].parentElement.classList.remove('open');
+      } else {
+        vyjizdecHeader[i].parentElement.classList.add('open');
+        vyjizdecHeader[i].parentElement.style.setProperty('left', '0');
+      }
+    });
+  };
+
+});
