37a8c2df2dab88f365832280e6861ce6cfffda0d
unknown <burkekbur>
2020-11-22 16:43:41 +0100

    Tisk i na mobilu

--- plugins/print/print.js
+++ plugins/print/print.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function (event) {
 
-  if (window.innerWidth > 750) {
     let printer = document.createElement('tisk');
     printer.id = 'print';
     printer.innerHTML = `<link rel="stylesheet" href="plugins/print/print.css">
@@ -10,6 +9,5 @@ document.addEventListener('DOMContentLoaded', function (event) {
     printer.addEventListener('click',function(){
       window.print();
     })
-  }
 
 })
